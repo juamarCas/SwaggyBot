4608ea8e377ba952d0ed76ce2f6a99ea6c296f77
Juan Martin <juanmartincas@gmail.com>
2020-05-29 11:21:22 -0500

    Added .env.example

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
-const { Client, MessageEmbed, MessageAttachment, VoiceChannel, Guild } = require("discord.js");
+const {
+  Client,
+  MessageEmbed,
+  MessageAttachment,
+  VoiceChannel,
+  Guild,
+} = require("discord.js");
 const giphy = require("./actions/giphy");
 const audios = require("./actions/audios");
-const flip = require('./actions/flipAcoin'); 
+const flip = require("./actions/flipAcoin");
 const fs = require("fs");
 const path = require("path");
 require("dotenv").config();
 const DiscordToken = process.env.DISCORD_TOKEN;
 const client = new Client();
-const guild = new Guild(client, Object()); 
+const guild = new Guild(client, Object());
 var color = "0xff0000";
 var newColor = "0x00ff00";
 const PREFIX = "-";
 
-
 client.on("ready", () => {
   console.log(`logged in as ${client.user.tag}`); //este método es, cuando el bot esté listo, ejecute la función
-
 });
 
 client.on("message", (message) => {
@@ -32,32 +36,35 @@ client.on("message", (message) => {
           .setColor("0xff0000")
           .setDescription(buf.toString());
         message.channel.send(embed);
-        // fs.close("commands.txt");
       });
+      // fs.close("commands.txt");
       break;
 
     case `${PREFIX}flip`:
-      var cc = flip.tossCoin(); 
+      var cc = flip.tossCoin();
       const text = `${message.author} flipped a coin and the result is: ${cc}`;
       const tags = "flip a coin";
-      let embed = new MessageEmbed(); 
+      let embed = new MessageEmbed();
       giphy.SendGIF(text, tags, color, message, embed);
       break;
 
     case `${PREFIX}wololo`:
       const directory = path.join(__dirname, "gifs", "wololo.gif");
       var attatchment = new MessageAttachment(directory);
-      message.channel.send(`${message.author} ha wololoseado el servidor`); 
+      message.channel.send(`${message.author} ha wololoseado el servidor`);
       message.channel.send(attatchment);
       break;
 
     case `${PREFIX}o`:
       var voiceChannel = new VoiceChannel(guild, Object());
-      voiceChannel.join().then((connection) => {
-        console.log("Connected");
-      }).catch((e) => {
-        console.log("Error"); 
-      });
+      voiceChannel
+        .join()
+        .then((connection) => {
+          console.log("Connected");
+        })
+        .catch((e) => {
+          console.log("Error");
+        });
       break;
   }
 
@@ -74,11 +81,10 @@ client.on("message", (message) => {
     if (user) {
       const text = `${message.author} le pegó una trompá a ${user}`;
       const tags = "onepunchman, onepunch, saitama, anime";
-      let embed = new MessageEmbed(); 
+      let embed = new MessageEmbed();
       giphy.SendGIF(text, tags, color, message, embed);
     }
   }
 });
-
 
 client.login(DiscordToken);
